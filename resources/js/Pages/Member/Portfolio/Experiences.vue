994caff2362c1cc6ec7c5d2e35a579dc9501fd30
josechanfm <josechan@ipm.edu.mo>
2024-09-26 12:27:00 +0800

    experiences modal

--- resources/js/Pages/Member/Portfolio/Experiences.vue
+++ resources/js/Pages/Member/Portfolio/Experiences.vue
@@ -1,69 +1,80 @@
 <script setup>
 import { ref } from 'vue';
 import { useForm, router } from '@inertiajs/vue3';
+import { CheckCircleOutlined } from '@ant-design/icons-vue';
+
+const dateFormat='YYYY-MM-DD';
 
 const props = defineProps({
   experiences: Array,
   portfolioId: Number,
 });
 
-const editModalVisible = ref(false);
-const addModalVisible = ref(false);
+const modalVisible = ref(false);
+const modalState = ref('CREATE');
 
 const form = useForm({
   id: null,
-  position: '',
+  title: '',
   company: '',
-  duration: '',
+  start_date: '',
+  end_date: '',
+  is_current: false,
+  description: '',
 });
 
 const editExperience = (experience) => {
   console.log('editExperience function called', experience);
   form.id = experience.id;
-  form.position = experience.position;
+  form.title = experience.title;
   form.company = experience.company;
-  form.duration = experience.duration;
-  editModalVisible.value = true;
+  form.start_date = experience.start_date;
+  form.end_date = experience.end_date;
+  form.is_current = experience.is_current;
+  form.description = experience.description;
+  modalState.value = 'EDIT';
+  modalVisible.value = true;
 };
 
 const addExperience = () => {
   console.log('addExperience function called');
   form.reset();
-  addModalVisible.value = true;
+  modalState.value = 'CREATE';
+  modalVisible.value = true;
 };
 
-const cancelEdit = () => {
-  console.log('cancelEdit function called');
-  editModalVisible.value = false;
-  form.reset();
-};
-
-const cancelAdd = () => {
-  console.log('cancelAdd function called');
-  addModalVisible.value = false;
+const cancelModal = () => {
+  console.log('cancelModal function called');
+  modalVisible.value = false;
   form.reset();
 };
 
 const saveExperience = () => {
-  console.log('saveExperience function called', form.data());
-  router.put(route('member.portfolio.experiences.update', [props.portfolioId, form.id]), form.data(), {
-    preserveScroll: true,
-    onSuccess: () => {
-      editModalVisible.value = false;
-      form.reset();
-    },
-  });
-};
+  const formData = {
+    ...form.data(),
+    start_date: form.start_date ? dayjs(form.start_date).tz("Asia/Macao").format('YYYY-MM-DD') : null,
+    end_date: form.end_date ? dayjs(form.end_date).tz("Asia/Macao").format('YYYY-MM-DD') : null,
+  };
 
-const createExperience = () => {
-  console.log('createExperience function called', form.data());
-  router.post(route('member.portfolio.experiences.store', props.portfolioId), form.data(), {
-    preserveScroll: true,
-    onSuccess: () => {
-      addModalVisible.value = false;
-      form.reset();
-    },
-  });
+  if (modalState.value === 'EDIT') {
+    console.log('saveExperience function called', formData);
+    router.put(route('member.portfolio.experiences.update', [props.portfolioId, form.id]), formData, {
+      preserveScroll: true,
+      onSuccess: () => {
+        modalVisible.value = false;
+        form.reset();
+      },
+    });
+  } else {
+    console.log('createExperience function called', formData);
+    router.post(route('member.portfolio.experiences.store', props.portfolioId), formData, {
+      preserveScroll: true,
+      onSuccess: () => {
+        modalVisible.value = false;
+        form.reset();
+      },
+    });
+  }
 };
 
 const deleteExperience = (experience) => {
@@ -90,6 +101,11 @@ const columns = [
     key: 'duration',
   },
   {
+    title: 'Current',
+    dataIndex: 'is_current',
+    key: 'is_current',
+  },
+  {
     title: 'Action',
     key: 'action',
   },
@@ -103,7 +119,10 @@ const columns = [
     </template>
     <a-table :dataSource="experiences" :columns="columns">
       <template #bodyCell="{ column, record }">
-        <template v-if="column.key === 'action'">
+        <template v-if="column.key === 'is_current'">
+          <CheckCircleOutlined v-if="record.is_current" style="color: #52c41a; font-size: 18px;" />
+        </template>
+        <template v-else-if="column.key === 'action'">
           <a-button type="primary" @click="editExperience(record)" class="mr-2">Edit</a-button>
           <a-popconfirm
             title="Are you sure you want to delete this experience?"
@@ -117,42 +136,40 @@ const columns = [
       </template>
     </a-table>
 
-    <!-- Edit Experience Modal -->
+    <!-- Experience Modal -->
     <a-modal
-      v-model:open="editModalVisible"
-      title="Edit Experience"
+      v-model:open="modalVisible"
+      :title="modalState === 'EDIT' ? 'Edit Experience' : 'Add Experience'"
       @ok="saveExperience"
-      @cancel="cancelEdit"
+      @cancel="cancelModal"
     >
-      <a-form :model="form">
-        <a-form-item label="Position">
-          <a-input type="input" v-model:value="form.position" />
+      <a-form :model="form" layout="vertical">
+        <a-form-item label="Position" name="title">
+          <a-input type="input" v-model:value="form.title" />
         </a-form-item>
-        <a-form-item label="Company">
+        <a-form-item label="Company" name="company">
           <a-input type="input" v-model:value="form.company" />
         </a-form-item>
-        <a-form-item label="Duration">
-          <a-input type="input" v-model:value="form.duration" />
+        <a-form-item label="Start Date" name="start_date">
+          <a-date-picker 
+            v-model:value="form.start_date" 
+            style="width: 100%" 
+            :format="dateFormat" :valueFormat="dateFormat"
+          />
         </a-form-item>
-      </a-form>
-    </a-modal>
-
-    <!-- Add Experience Modal -->
-    <a-modal
-      v-model:open="addModalVisible"
-      title="Add Experience"
-      @ok="createExperience"
-      @cancel="cancelAdd"
-    >
-      <a-form :model="form">
-        <a-form-item label="Position">
-          <a-input type="input" v-model:value="form.position" />
+        <a-form-item label="End Date" name="end_date">
+          <a-date-picker 
+            v-model:value="form.end_date" 
+            style="width: 100%" 
+            :disabled="form.is_current"
+            :format="dateFormat" :valueFormat="dateFormat"
+          />
         </a-form-item>
-        <a-form-item label="Company">
-          <a-input type="input" v-model:value="form.company" />
+        <a-form-item name="is_current">
+          <a-checkbox v-model:checked="form.is_current">Currently working here</a-checkbox>
         </a-form-item>
-        <a-form-item label="Duration">
-          <a-input type="input" v-model:value="form.duration" />
+        <a-form-item label="Description" name="description">
+          <a-textarea v-model:value="form.description" :rows="4" />
         </a-form-item>
       </a-form>
     </a-modal>
